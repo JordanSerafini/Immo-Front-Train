a800f59da0253e2500a99b7cc74bee5660e61037
Loris QUESADO <loris.quesado@oclock.school>
2023-09-21 10:14:26 +0200

    Architecture: Tailwind config - Assets - Fonts

---
+++ src/components/App/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <p className="text-3xl bg-primary-300 shadow-custom">ImmoPros</p>
+    </div>
+  );
+}
+
+export default App;
