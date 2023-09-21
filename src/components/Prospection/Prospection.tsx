3451f76e9eae305b0959a8d8342d90bdbae9abeb
Loris QUESADO <loris.quesado@oclock.school>
2023-09-21 19:52:14 +0200

    :feat: Prospection - Start of integration

--- src/components/Prospection/Prospection.tsx
+++ src/components/Prospection/Prospection.tsx
@@ -1,10 +1,23 @@
+// Components
 import NavBar from '../NavBar/NavBar';
+
+// Assets
+import logo from '../../assets/logo.svg';
+import search from '../../assets/icons/search.svg';
 
 export default function Prospection() {
   return (
-    <div className='flex w-screen h-screen'>
+    <div className="flex w-screen h-screen">
       <NavBar />
-      <main className='grow'>Prospection</main>
+      <main className="m-5 grow">
+        <img src={logo} alt="Logo Immo'Pros" />
+        <h1 className="mt-20 text-xl font-semibold text-center font-poppins">
+          Informations de prospection
+        </h1>
+        <fieldset className='relative'>
+          <input type="text" placeholder="Effectuer votre recherche..." />
+        </fieldset>
+      </main>
     </div>
   );
 }
