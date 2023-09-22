6a1d2de38f6ec4a85923464b04ab20b34dda791f
Loris QUESADO <loris.quesado@oclock.school>
2023-09-22 16:28:33 +0200

    :feat: AddInfoModal - In progress

--- src/components/Modals/AddInfoModal/AddInfoModal.tsx
+++ src/components/Modals/AddInfoModal/AddInfoModal.tsx
@@ -2,6 +2,19 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 
+// React
+import { FormEvent } from 'react';
+
+// Components
+import Fieldset from '../Form/Fieldset';
+
+// Assets
+import plus from '../../../assets/icons/plus.svg';
+
+// Style
+import './animation.scss';
+
+// Typescript Interface
 interface AddInfoModalProps {
   // Not sure if the type is good here. It seems too simple...
   closeModal: () => void;
@@ -15,6 +28,10 @@ export default function AddInfoModal({ closeModal }: AddInfoModalProps) {
     }
   };
 
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <dialog
       className="fixed inset-0 z-30 flex items-center justify-center w-screen h-screen m-0 border-none bg-black/60"
@@ -24,7 +41,7 @@ export default function AddInfoModal({ closeModal }: AddInfoModalProps) {
       <div
         role="dialog"
         onClick={(event) => event.stopPropagation()}
-        className="relative w-24 rounded-xl aspect-square bg-secondary-50"
+        className="relative w-full p-4 mx-4 slide rounded-xl bg-secondary-50"
       >
         {/* Temporary style */}
         <button
@@ -32,8 +49,71 @@ export default function AddInfoModal({ closeModal }: AddInfoModalProps) {
           type="button"
           className="absolute top-2 right-2"
         >
-          X
+          <img
+            className="duration-300 rotate-45 rounded-full bg-primary-300 hover:bg-primary-500"
+            src={plus}
+            alt="Plus Icon"
+          />
         </button>
+
+        <h1 className="my-5 text-2xl font-semibold text-center font-poppins">
+          Ajout d&apos;une information
+        </h1>
+
+        <em className="italic">*Champs obligatoires</em>
+
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col items-center justify-center gap-6 mb-4"
+        >
+          <Fieldset title="*Type de bien">
+            <div>
+              <div>
+                <input
+                  type="radio"
+                  id="maison"
+                  name="drone"
+                  value="maison"
+                  defaultChecked
+                />
+                <label htmlFor="maison">Maison</label>
+              </div>
+
+              <div>
+                <input type="radio" id="appartement" name="drone" value="appartement" />
+                <label htmlFor="appartement">Appartement</label>
+              </div>
+
+              <div>
+                <input type="radio" id="Terrain" name="drone" value="Terrain" />
+                <label htmlFor="Terrain">Terrain</label>
+              </div>
+            </div>
+          </Fieldset>
+
+          <Fieldset title='*Localisation'>
+            <div>
+              <input type="text" placeholder='N°' />
+            </div>
+          </Fieldset>
+
+          <div className="flex justify-between w-3/4 mt-5">
+            <button
+              type="button"
+              className="px-6 py-2 duration-300 rounded-lg text-secondary-50 bg-primary-300 font-poppins lg:text-lg hover:shadow-primary"
+            >
+              Enregistrer
+            </button>
+            <button
+              type="button"
+              className="p-[3px] duration-300 rounded-md bg-gradient-to-tr from-[#1A44FF] to-[#944DFF] hover:shadow-accent"
+            >
+              <div className="flex items-center justify-center w-full h-full px-4 font-semibold rounded lg:text-lg bg-secondary-50">
+                Annuler
+              </div>
+            </button>
+          </div>
+        </form>
       </div>
     </dialog>
   );
