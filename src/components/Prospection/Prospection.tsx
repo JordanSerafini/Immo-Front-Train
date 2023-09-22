6e31dd6ef88b33df96f82b135147188ae7928b38
Loris QUESADO <loris.quesado@oclock.school>
2023-09-22 13:33:33 +0200

    :feat: Propsection - Filter input (better responsive behavior)

--- src/components/Prospection/Prospection.tsx
+++ src/components/Prospection/Prospection.tsx
@@ -21,7 +21,7 @@ export default function Prospection() {
         <h1 className="mt-20 text-xl font-semibold text-center font-poppins md:text-3xl">
           Informations de prospection
         </h1>
-        <fieldset className="relative z-0 block mx-auto mt-3 mb-6 w-fit">
+        <fieldset className="relative z-0 block mx-auto mt-3 mb-6 w-fit md:mx-0 md:mb-3 md:mt-6">
           <input
             className="py-2 pl-10 duration-150 rounded-lg w-72 shadow-custom font-poppins focus:ring-4 ring-accent-300"
             type="text"
@@ -39,8 +39,14 @@ export default function Prospection() {
           type="button"
           className="fixed flex items-center justify-center w-12 p-1 duration-300 rounded-full aspect-square bg-primary-300 hover:shadow-primary hover:scale-110 bottom-7 right-10 sm:static sm:rounded-lg sm:aspect-auto sm:mb-4 sm:pr-4 sm:w-fit sm:p-2"
         >
-          <img src={plus} alt="Add Info Button Icon" className="w-full sm:w-[30px]" />
-          <span className="hidden text-secondary-50 font-poppins sm:inline">Ajouter une information</span>
+          <img
+            src={plus}
+            alt="Add Info Button Icon"
+            className="w-full sm:w-[30px]"
+          />
+          <span className="hidden text-secondary-50 font-poppins sm:inline">
+            Ajouter une information
+          </span>
         </button>
 
         <div className="flex-wrap justify-between pb-2 lg:flex">
