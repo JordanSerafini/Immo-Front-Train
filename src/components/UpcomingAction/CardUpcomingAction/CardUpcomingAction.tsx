2f1867cba1626369382cf4f6a7fc1e3b3e0a35b4
Loris QUESADO <loris.quesado@oclock.school>
2023-09-24 14:51:23 +0200

    :sparkles: UpcomingAction Page - With his Card Component

--- src/components/UpcomingAction/CardUpcomingAction/CardUpcomingAction.tsx
+++ src/components/UpcomingAction/CardUpcomingAction/CardUpcomingAction.tsx
@@ -1,5 +1,46 @@
-export default function CardUpcomingAction() {
+// Assets
+import houseIcon from '../../../assets/icons/house.svg';
+import apartmentIcon from '../../../assets/icons/apartment.svg';
+import landIcon from '../../../assets/icons/land.svg';
+
+// Typescript interface
+interface CardUpcomingActionProps {
+  address: string;
+  owner: string;
+  type: string;
+  notificationDate: string;
+}
+
+// !! REFACTO WITH THE CardActionToDo COMPONENT !!
+export default function CardUpcomingAction({
+  address,
+  owner,
+  type,
+  notificationDate,
+}: CardUpcomingActionProps) {
+  let icon: string;
+  switch (type) {
+    case 'maison':
+      icon = houseIcon;
+      break;
+    case 'appartement':
+      icon = apartmentIcon;
+      break;
+    case 'terrain':
+      icon = landIcon;
+      break;
+    default:
+      icon = '';
+  }
+
   return (
-    <div>CardUpcomingAction</div>
-  )
+    <li className="flex items-start justify-between gap-5 p-5 rounded-lg bg-secondary-50 shadow-custom">
+      <div className="flex flex-col gap-2">
+        <img src={icon} alt={`${icon} Icon`} className="w-[25px] md:w-[30px]" />
+        <p className="md:text-lg">{address}</p>
+        <p className="md:text-lg">{owner}</p>
+      </div>
+      <p className='text-lg font-semibold text-center'><em className='italic'>A traiter le {notificationDate}</em></p>
+    </li>
+  );
 }
--- src/components/UpcomingAction/UpcomingAction.tsx
+++ src/components/UpcomingAction/UpcomingAction.tsx
@@ -1,10 +1,73 @@
+// React Router
+import { Link } from 'react-router-dom';
+
+// Components
 import NavBar from '../NavBar/NavBar';
+import CardUpcomingAction from './CardUpcomingAction/CardUpcomingAction';
+
+// Assets
+import logo from '../../assets/logo.svg';
+import upcomingActionIcon from '../../assets/icons/upcoming-action.svg';
 
 export default function UpcomingAction() {
   return (
     <>
       <NavBar />
-      <div>UpcomingAction</div>
+      <main className="m-5 md:m-10 grow">
+      <Link to="/app/prospection">
+          <img src={logo} alt="Logo Immo'Pros" className="sm:hidden" />
+        </Link>
+
+        <div className="flex flex-col items-center justify-center gap-2 mb-6">
+          <img
+            src={upcomingActionIcon}
+            alt="Action to do Icon"
+            className="w-[50px]"
+          />
+          <h1 className="text-xl font-semibold text-center font-poppins md:text-3xl">
+            Actions à venir
+          </h1>
+        </div>
+
+        <ul className="flex flex-col gap-5">
+          <CardUpcomingAction
+            address="5, rue de la Liberté 95190 GOUSSAINVILLE"
+            owner="Mr et Mme AKHTAR"
+            type="maison"
+            notificationDate='20/02/2023'
+          />
+          <CardUpcomingAction
+            address="25, boulevard Roger Salengro 95190 GOUSSAINVILLE"
+            owner="Mr et Mme DUCHAUFFOUR"
+            type="maison"
+            notificationDate='22/02/2023'
+          />
+          <CardUpcomingAction
+            address="12, rue du Montoir Saint-Nicolas 95190 FONTENAY-EN-PARISIS"
+            owner="Mr VIFFRY"
+            type="appartement"
+            notificationDate='25/02/2023'
+          />
+          <CardUpcomingAction
+            address="43, rue Lucien Mèche 95190 GOUSSAINVILLE"
+            owner="Mr ALCARAZ"
+            type="terrain"
+            notificationDate='02/03/2023'
+          />
+          <CardUpcomingAction
+            address="43, rue Lucien Mèche 95190 GOUSSAINVILLE"
+            owner="Mr ALCARAZ"
+            type="terrain"
+            notificationDate='02/03/2023'
+          />
+          <CardUpcomingAction
+            address="43, rue Lucien Mèche 95190 GOUSSAINVILLE"
+            owner="Mr ALCARAZ"
+            type="terrain"
+            notificationDate='02/03/2023'
+          />
+        </ul>
+      </main>
     </>
   );
 }
