b449078ff044bcf5ebd9dfafaf8e68eb7e371d2b
Loris QUESADO <loris.quesado@oclock.school>
2023-10-02 13:41:18 +0200

    :art: SectorCard - CSS adjustments

--- src/components/DashBoard/DashBoard.tsx
+++ src/components/DashBoard/DashBoard.tsx
@@ -1,5 +1,11 @@
 export default function DashBoard() {
   return (
-    <div>DashBoard</div>
-  )
+    <>
+      <h1 className="mt-20 mb-5 lg:mt-10">Dashboard</h1>
+
+      <section className="block mt-[30vh]">
+        <h2 className="text-center">Dashboard incoming with ChartJS</h2>
+      </section>
+    </>
+  );
 }
--- src/components/Panel/SectorCard/SectorCard.tsx
+++ src/components/Panel/SectorCard/SectorCard.tsx
@@ -6,22 +6,22 @@ import { Sector } from '../../../@types/sector';
 
 export default function SectorCard({ city, code_zip, color_code }: Sector) {
   return (
-    <article className="grid justify-around grid-cols-4 gap-8 p-5 my-5 rounded-lg bg-secondary-50 shadow-custom">
+    <article className="grid items-center justify-center grid-cols-2 gap-8 p-5 my-5 rounded-lg lg:grid-cols-4 bg-secondary-50 shadow-custom">
       <div className="flex flex-col items-center gap-4">
         <h3>Ville</h3>
-        <p>{city.toUpperCase()}</p>
+        <p className='text-center'>{city.toUpperCase()}</p>
       </div>
 
       <div className="flex flex-col items-center gap-4">
-        <h3>Code Postal</h3>
-        <p>{code_zip}</p>
+        <h3 className='text-center'>Code Postal</h3>
+        <p className='text-center'>{code_zip}</p>
       </div>
 
       <div className="flex flex-col items-center gap-4">
-        <h3 className="flex gap-2">
+        <h3 className="flex gap-2 text-center">
           Attribué à <img src={pencilIcon} alt="Pencil Icon" />
         </h3>
-        <p>Amandine LEROUX</p>
+        <p className='text-center'>Amandine LEROUX</p>
       </div>
 
       <div className="flex flex-col items-center gap-4">
