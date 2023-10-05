c7b8c0f53be8c120165c7e0ab8a980c70783545e
Loris QUESADO <loris.quesado@oclock.school>
2023-10-05 13:52:27 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Le répertoire de sortie pour les fichiers de production
+    rollupOptions: {
+      // L'option "output" spécifie le format de sortie pour la production
+      output: {
+        format: 'es', // Format ES Module
+      },
+    },
+  },
 });
