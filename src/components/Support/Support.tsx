096130b52689129866a9c35606af93b9c4c65889
dan3x90 <dan3x90@gmail.com>
2023-10-06 02:51:42 +0200

    Support page + SupportConfirmation page

--- src/components/Support/Support.tsx
+++ src/components/Support/Support.tsx
@@ -1,7 +1,75 @@
+// React Hooks
+import { FormEvent, useState } from 'react';
+
+// Redux
+import { useAppDispatch } from '../../hooks/redux';
+
+// Components
+import Logo from '../SharedComponents/Logo/Logo';
+import Textarea from '../Modals/AddInfoModal/Field/Textarea';
+import ValidButton from '../SharedComponents/Buttons/ValidButton';
+import Input from '../Modals/AddInfoModal/Field/Input';
+import SupportFooter from './SupportFooter/SupportFooter';
+
 export default function Support() {
+  // Local States
+  const [objectValue, setObjectValue] = useState<string>('');
+  const [message, setMessage] = useState<string>('');
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+
+    console.log(Object.fromEntries(formData));
+  };
+
   return (
-    <div className="flex items-center justify-center w-full h-full">
-      <h1>PAGE SUPPORT EN COURS DE DEVELOPPEMENT</h1>
-    </div>
+    <>
+      {/* LOGO */}
+      <Logo
+        path="/"
+        className="absolute top-5 left-5"
+      />
+
+      <main className="flex flex-col w-full h-full">
+        {/* TITLE */}
+        <h1 className="mt-40">Un soucis ?</h1>
+        <h1 className="mb-20">
+          Pas de panique ! Contactez le support technique
+        </h1>
+
+        {/* OBJET */}
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-xl mx-auto font-poppins"
+        >
+          <Input
+            value={objectValue}
+            onChange={setObjectValue}
+            inputName="object"
+            placeholder="Renseignez l'objet de votre demande"
+            label="Objet"
+          />
+
+          {/* OBJECT MESSAGE */}
+          <Textarea
+            value={message}
+            onChange={setMessage}
+            textareaName="message"
+            placeholder="Votre message..."
+          />
+
+          {/* SEND BUTTON */}
+          <ValidButton
+            content="Se connecter"
+            isSubmit
+            className="w-full mt-10"
+          />
+        </form>
+        <SupportFooter />
+      </main>
+    </>
   );
 }
