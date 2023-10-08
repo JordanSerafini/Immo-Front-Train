f05f8160e8b00cf17ce64265ba88b4d02b8491df
Loris QUESADO <loris.quesado@oclock.school>
2023-10-08 14:05:08 +0200

    :sparkles: Edit Sector Color

--- src/components/SectorManager/SectorCard/SectorCard.tsx
+++ src/components/SectorManager/SectorCard/SectorCard.tsx
@@ -9,6 +9,7 @@ import { findCollaborator } from '../../../store/selectors/collaborator';
 
 // Components
 import AttributionForm from './AttributionForm';
+import ColorForm from './ColorForm';
 
 // Assets
 import pencilIcon from '../../../assets/icons/edit-pencil.svg';
@@ -26,6 +27,7 @@ export default function SectorCard({
 }: Sector) {
   // Local State
   const [editingAffectation, setEditingAffection] = useState<boolean>(false);
+  const [editingColor, setEditingColor] = useState<boolean>(false);
 
   // Selector
   const collaborator = useAppSelector(findCollaborator(collaborator_id));
@@ -43,6 +45,10 @@ export default function SectorCard({
   // handlers
   const affectSector = () => {
     setEditingAffection(!editingAffectation);
+  };
+
+  const editColor = () => {
+    setEditingColor(!editingColor);
   };
 
   return (
@@ -79,14 +85,29 @@ export default function SectorCard({
         )}
       </div>
 
-      <div className="flex flex-col items-center gap-4">
+      <div className="relative flex flex-col items-center gap-4">
         <h3 className="flex gap-2">
-          Couleur <img src={pencilIcon} alt="Pencil Icon" />
+          Couleur
+          <button type="button" onClick={editColor}>
+            <img src={pencilIcon} alt="Pencil Icon" />
+          </button>
         </h3>
         <div
           className="w-[50px] aspect-square rounded-full shadow-custom"
           style={{ backgroundColor: color_code }}
         />
+
+        {editingColor && (
+          <ColorForm
+            id={id}
+            city={city}
+            code_zip={code_zip}
+            color_code={color_code}
+            label={label}
+            setState={setEditingColor}
+            collaborator_id={collaborator_id}
+          />
+        )}
       </div>
     </article>
   );
