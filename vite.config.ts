e46c8feccfbdece480011f5eef7e7570843d8e53
Loris QUESADO <loris.quesado@oclock.school>
2023-10-11 14:57:58 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,15 @@ const PORT = Number(process.env.PORT) || 3000;
 
 // https://vitejs.dev/config/
 export default vite.defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
   server: {
     port: PORT,
   },
+
   build: {
+    minify: 'terser',
     outDir: 'dist', // Le répertoire de sortie pour les fichiers de production
     rollupOptions: {
       // L'option "output" spécifie le format de sortie pour la production
