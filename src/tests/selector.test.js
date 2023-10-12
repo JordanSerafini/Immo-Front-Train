58ea5719b10e3305740f2220f44b3d5bc08b3054
Loris QUESADO <loris.quesado@oclock.school>
2023-10-12 14:22:02 +0200

    :bug: (Leaflet) Icon Fix & Test adjusments for ts

--- src/tests/selector.test.js
+++ src/tests/selector.test.js
@@ -1,9 +1,6 @@
 import { describe, expect, expectTypeOf, it } from 'vitest';
 import { findInformation } from '../store/selectors/information';
 import { findCollaborator } from '../store/selectors/collaborator';
-// === TYPESCRIPT === //
-import { User } from '../@types/user';
-import { Information } from '../@types/information';
 
 // === DATA === //
 import collaboratorsData from '../data/collaboratorsData';
@@ -30,15 +27,6 @@ describe('findInformation function', () => {
     it('Should be a function', () => {
       expectTypeOf(findInformation).toBeFunction();
     });
-
-    it('Should return an Information object if found or undefined', () => {
-      const found = findInformation('1')(fakeInformationsData);
-
-      expectTypeOf(found).toMatchTypeOf<Information | undefined>();
-
-      const notFound = findInformation('99')(fakeInformationsData);
-      expectTypeOf(notFound).toMatchTypeOf<Information | undefined>();
-    });
   });
 
   describe('Execution', () => {
@@ -59,15 +47,6 @@ describe('findCollaborator function', () => {
   describe('Structure', () => {
     it('Should be a function', () => {
       expectTypeOf(findCollaborator).toBeFunction();
-    });
-
-    it('Should return a Collaborator object if found or undefined', () => {
-      const found = findCollaborator(2)(fakeCollaboratorData);
-
-      expectTypeOf(found).toMatchTypeOf<User | undefined>();
-
-      const notFound = findCollaborator(99)(fakeCollaboratorData);
-      expectTypeOf(notFound).toMatchTypeOf<User | undefined>();
     });
   });
 
