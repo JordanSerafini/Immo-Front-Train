984173986da2d6da6a9ffa45b7f719008e73ff77
Loris QUESADO <loris.quesado@oclock.school>
2023-10-12 10:52:33 +0200

    :white_check_mark: Format Functions Test

--- src/utils/formatPhone.ts
+++ src/utils/formatPhone.ts
@@ -1,8 +1,10 @@
 export default function formatPhone(phone: string) {
   let formatedPhone = '';
 
+  if (!phone.length || phone.length < 10) return formatedPhone;
+
   // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < phone.length; i++) {
+  for (let i = 0; i < 10; i++) {
     if (i > 0 && i % 2 === 0) {
       formatedPhone += '.';
     }
